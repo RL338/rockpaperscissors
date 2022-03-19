00436c4246257a8803173bd487f127ca48e45b48
Ryan L <ryan.lai011@gmail.com>
2022-03-18 23:42:15 -0700

    input rock paper or scissors

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-const choices = {"Rock", "Paper", "Scissors"}
+function game(){
+  playRound();
+}
+
+function playRound(){
+  let playerSelection = playerChoice();
+  let computerSelection = computerChoice();
+}
+
+let choices = ["Rock", "Paper", "Scissors"];
+
+
 
 function computerChoice(){
-  return choices[Math.floor(Math.random()*choices.length)]
+  return choices[Math.floor(Math.random()* choices.length)]
 }
+
 
 function playerChoice(){
   let input = prompt("Type Rock, Paper, or Scissors");
@@ -11,18 +23,11 @@ function playerChoice(){
   }
 }
 
-function computerPlay(){
-  if (Math.floor(Math.random() * 3)) =0 {
-    return "Rock";
-  } else if (Math.floor(Math.random() * 3)) =1){
-    return "Paper";
-  }
-    else if (Math.floor(Math.random() * 3)) =2{
-      return "Scissors";
-    }
-}
+
 
 function play(playerSelection, computerSelection){
    console.log(playerSelection)
 }
 const PlayerChoice = "Rock"
+
+game();
